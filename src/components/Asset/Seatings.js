7113e2f93dd4a76082de3362d9c8c6535b5574e7
Leung <andrew_leung@cable.comcast.net>
2023-01-13 21:59:36 -0500

    use mainnet

--- src/components/Asset/Seatings.js
+++ src/components/Asset/Seatings.js
@@ -5,15 +5,18 @@ import { db } from '../tasks/firebase';
 import './Seatings.css';
 
 import { deployed } from '../../config';
-import Market from '../../artifacts/contracts/Market.sol/NFTMarket.json';
+// import Market from '../../artifacts/contracts/Market.sol/NFTMarket.json';
+import NFT from '../../artifacts/contracts/NFT.sol/NFT.json';
 
 const envChainName = deployed.envChain.name;
 const envChainId = deployed.envChain.id;
-const { nftmarketaddress, nftaddress } = deployed;
+const { nftaddress } = deployed;
 
 export default function Seatings({ title }) {
   const [task, setTask] = useState();
   const [players, setPlayers] = useState([]);
+  const [message, setMessage] = useState('');
+  const [cause, setCause] = useState('');
 
   /* function to get all tasks from firestore in realtime */
   useEffect(() => {
@@ -41,11 +44,20 @@ export default function Seatings({ title }) {
   const placeBid = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const market = new ethers.Contract(nftmarketaddress, Market.abi, signer);
-    const price = ethers.utils.parseUnits('0.01', 'ether');
+    // const market = new ethers.Contract(nftmarketaddress, Market.abi, signer);
+    // const price = ethers.utils.parseUnits('0.01', 'ether');
     try {
-      const transaction = await market.createMarketSale(nftaddress, 2, { value: price });
-      await transaction.wait();
+      // const transaction = await market.createMarketSale(nftaddress, 2, { value: price });
+      // await transaction.wait();
+
+      const contract = new ethers.Contract(nftaddress, NFT.abi, signer);
+      const transaction = await contract.createToken('tokenURIHash', 'content', 0);
+      const tx = await transaction.wait();
+      const event = tx.events[0];
+      console.log({ event });
+      // const value = event.args[2];
+      // const tokenId = value.toNumber();
+      // console.log({ tokenId });
     } catch (error) {
       if (error.code === -32603) {
         console.error({ title: 'Error - Please check your wallet and try again.', message: 'It is very possible that the RPC endpoint you are using to connect to the network with MetaMask is congested or experiencing technical problems' });
@@ -62,35 +74,34 @@ export default function Seatings({ title }) {
         window.ethereum.request({ method: 'eth_chainId' }),
       ]).catch((error) => {
         if (error.code === 4001) {
-          throw new Error('Please check your wallet and try again', { message: 'Connection request has been rejected. ' });
+          throw new Error('Please check your wallet and try again', { cause: 'Connection request has been rejected. ' });
         } else if (error.code === -32002) {
-          throw new Error('Please check your wallet and try again', { message: error.message });
+          throw new Error('Please check your wallet and try again', { cause: error.message });
         } else {
-          throw new Error('Please check your wallet and try again', { message: error.message });
+          throw new Error('Please check your wallet and try again', { cause: error.message });
         }
       });
       if (result) {
         const [accounts, chainId] = result;
         console.log('error1', result, envChainId);
         if (accounts.length === 0) {
-          throw new Error('Please check your wallet and try again', { message: 'MetaMask is locked or the user has not connected any accounts' });
+          throw new Error('Please check your wallet and try again', { cause: 'MetaMask is locked or the user has not connected any accounts' });
         }
         if (chainId !== envChainId) {
-          throw new Error('Please check your wallet and try again', { message: `Error - Is your wallet connected to ${envChainName}?` });
+          throw new Error('Please check your wallet and try again', { cause: `Error - Is your wallet connected to ${envChainName}?` });
         }
         return accounts[0];
       }
     }
-    throw new Error('Non-Ethereum browser detected.', { message: 'You should consider installing MetaMask' });
+    throw new Error('Non-Ethereum browser detected.', { cause: 'You should consider installing MetaMask' });
   }
 
   /* function to update firestore */
   const handleChange = async (player) => {
-    const account = await ethAccountsRequest();
-    placeBid();
-
-    const taskDocRef = doc(db, 'gamings', task.id);
     try {
+      const account = await ethAccountsRequest();
+      await placeBid();
+      const taskDocRef = doc(db, 'gamings', task.id);
       await updateDoc(taskDocRef, {
         players: arrayUnion(player),
         seats: arrayUnion({ account, player }),
@@ -98,6 +109,8 @@ export default function Seatings({ title }) {
       setPlayers([...players, player]);
     } catch (err) {
       console.error(err);
+      setMessage(err.message);
+      setCause(err.cause);
     }
     return false;
   };
@@ -106,6 +119,8 @@ export default function Seatings({ title }) {
     <div className="theatre">
       <div className="screen-side">
         <h3 className="select-text">Please select your seat</h3>
+        <h1>{message}</h1>
+        <h1>{cause}</h1>
       </div>
       <ol className="cabin">
         {
