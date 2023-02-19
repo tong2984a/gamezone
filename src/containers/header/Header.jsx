00c57c5be8d2e3af1a246d3a5057c9b0621665a5
Leung <andrew_leung@cable.comcast.net>
2023-02-19 00:52:45 -0500

    new layout

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,9 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Greed Island</h1>
+      <div className="gpt3__text-heading">
+        <h1 className="gpt3__text">Greed Island</h1>
+      </div>
     </div>
   </div>
 );
